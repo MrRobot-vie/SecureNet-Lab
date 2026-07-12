534be88e59c7db33f3b8549035e8b23eeb481253
ANKRAH José <yawoankrah22@gmail.com>
2026-07-12 05:18:33 +0200

    Module 1 : ajout des noms de services

--- module1_scanner/port_scan.py
+++ module1_scanner/port_scan.py
@@ -1,6 +1,19 @@
 import socket
 import sys
-
+services = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    3306: "MySQL",
+    3389: "RDP"
+}
 from concurrent.futures import ThreadPoolExecutor 
 def tester_port(ip, port):
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,7 +21,8 @@ def tester_port(ip, port):
    resultat = sock.connect_ex((ip, port))
    sock.close()
    if resultat == 0:
-      print(f"Port {port} : OUVERT")
+      nom = services.get(port, "inconnu")
+      print(f"Port {port} : OUVERT ({nom})")
    
 ip = sys.argv[1]
 ports = range(1, 1025)
